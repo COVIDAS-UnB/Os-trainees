c2af3d0ddd4a51f304452ffb04d016b2ef264fd4
Léo Ribas <leoribasnasc@gmail.com>
2020-06-30 09:33:31 -0300

    💄 Adiciona templates de página

--- front-end/necto/src/pages/NotificationPage.js
+++ front-end/necto/src/pages/NotificationPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import NavBar from "../components/NavBar";
+import CheckinCard from "../components/CheckinCard";
 
 export default function Notification({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Notification Page</Text>
+      <CheckinCard />
       <NavBar navigation={navigation} />
     </View>
   );
