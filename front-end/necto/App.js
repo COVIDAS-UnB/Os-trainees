a2e6172b680789f76bd7930dc513fb02f2547280
Nataly Lacerda <nataly.oliveira@cjr.org.br>
2020-06-29 22:22:31 -0300

    cria página de sintomas

--- front-end/necto/App.js
+++ front-end/necto/App.js
@@ -1,26 +1,2 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-import NavBar from "./src/components/NavBar";
-import Header from "./src/components/Header";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Header title="Histórico" />
-      <Text>Open up App.js to start working on your app!</Text>
-      <NavBar />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-});
 
